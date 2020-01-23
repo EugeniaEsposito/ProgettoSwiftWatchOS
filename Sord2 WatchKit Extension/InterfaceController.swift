ec6512d2979fbbaa7b7283eb55cf198bd3f1dc8a
Eugenia <engy.espo@icloud.com>
2020-01-23 12:54:06 +0100

    Initial Commit

---
+++ Sord2 WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Sord2 WatchKit Extension
+//
+//  Created by Eugenia Esposito on 23/01/2020.
+//  Copyright © 2020 iOS Foundation. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Sord2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sord2
+//
+//  Created by Eugenia Esposito on 23/01/2020.
+//  Copyright © 2020 iOS Foundation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
